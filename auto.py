f8e2f520ed2f85542bf754fd8f20273680934b10
Lq-qm <30539207+Lq-qm@users.noreply.github.com>
2023-10-07 23:11:00 -0400

    Add files via upload

--- auto.py
+++ auto.py
@@ -1,3 +1,4 @@
+import os
 import string
 
 from selenium import webdriver
@@ -6,7 +7,10 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+options = webdriver.ChromeOptions()
+options.add_argument("--headless=new")
+
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 driver.get("https://chauthanh.info/animeost/browse/A.html")
 
 # Lista Geral
@@ -21,20 +25,33 @@ if letra in letras:
     print(link_letra)
 # Listagem dos albums de acordo com a letra escolhida
     driver.get(link_letra)
-    for contagem in range(1, 10000):
-        lista_letra = driver.find_element_by_xpath('//*[@id="content"]/div[3]/div[2]/span[%s]/a' % (contagem))
-        link_album = lista_letra.get_attribute('href')
-        print(link_album)      
+    with open('links.txt', 'w') as arquivo:
+        for contagem in range(1, 10000):
+            try:
+                lista_letra = driver.find_element_by_xpath('//*[@id="content"]/div[3]/div[2]/span[%s]/a' % (contagem))
+                link_album = lista_letra.get_attribute('href')
+                arquivo.write(link_album + '\n')
+            except Exception as e:
+                album = input(str("qual album voce quer? "))
+                os.system("grep -w" + album + "links.txt")
+                break     
     captura_link_album = input("cole o link do album: ")
 # Listagem das Musicas do Album Escolhido
     driver.get(captura_link_album)
     for contagem in range(1, 10000):
-        lista_album = driver.find_element_by_xpath('//*[@id="content"]/div[2]/table/tbody/tr[%s]/td[1]/a' % (contagem))
-        link_musica = lista_album.get_attribute('href')
-        driver.get(link_musica)
-        download_musica = driver.find_element_by_xpath('//*[@id="content"]/div[1]/div[2]/p/a[1]')
-        link_download_musica = download_musica.get_attribute('href')
-        print(link_download_musica)
-        driver.get(captura_link_album)
+        try:
+            driver.get(link_album)
+            lista_album = driver.find_element_by_xpath('//*[@id="content"]/div[2]/table/tbody/tr[%s]/td[1]/a' % (contagem))
+            link_musica = lista_album.get_attribute('href')
+            driver.get(link_musica)
+            download_musica = driver.find_element_by_xpath('//*[@id="content"]/div[1]/div[2]/p/a[1]')
+            link_download_musica = download_musica.get_attribute('href')
+            print(link_download_musica)
+            arquivo.write(link_download_musica + "\n") # Salvando o link da musica
+            driver.get(link_album)
+        except Exception as e:
+            print("links salvos em" + album + ".txt ")
+            driver.close()
+            break
 else:
     print("letra invalida")
